9b83a9dbfe1cc3dc4e8da3df71b6c414e304f53f
Andrey Antukh <niwi@niwi.be>
2014-09-13 09:32:21 +0200

    Fix tests to run with django 1.7

--- testing/runtests.py
+++ testing/runtests.py
@@ -3,8 +3,11 @@
 import os, sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
-from django.core.management import call_command
-
 if __name__ == "__main__":
-    args = sys.argv[1:]
-    call_command("test", *args, verbosity=2)
+    from django.core.management import execute_from_command_line
+
+    args = sys.argv
+    args.insert(1, "test")
+    args.insert(2, "pg_uuid_fields")
+
+    execute_from_command_line(args)
